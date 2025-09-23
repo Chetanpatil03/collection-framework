5e57786076257d7c082f52d4ee0e9d7f9be1512c
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-09-23 18:04:25 +0530

    problem solution

--- Questions/Solution.java
+++ Questions/Solution.java
@@ -10,15 +10,37 @@ class Solution {
 		Scanner sc = new Scanner(System.in);
 		String ch = sc.next();
 
-        if (ch.equals(ch.toUpperCase())) {
-            System.out.println(1);
+        // if(Integer.parseInt(ch) > 0){
+        //     System.out.println(-1);
+        // }
+        // else if (ch.equals(ch.toUpperCase())) {
+        //     System.out.println(1);
+        // }
+        // else if (ch.equals(ch.toLowerCase())){
+        //     System.out.println(0);
+        // }
+        // else{
+        //     System.out.println(-1);
+        // }
+
+        for (char c : ch.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                if (Character.isUpperCase(c)) {
+                    System.out.println(1);
+                }
+                else{
+                    System.out.println(0);
+                }
+            }
+            else if (Character.isDigit(c)){
+                System.out.println(-1);
+            }
+            else if (!Character.isLetterOrDigit(c)) {
+                System.out.println(-1);
+            }
         }
-        else if (ch.equals(ch.toLowerCase())){
-            System.out.println(0);
-        }
-        else{
-            System.out.println(-1);
-        }
+
+        sc.close();
 
 		
         
