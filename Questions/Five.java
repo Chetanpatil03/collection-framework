277335ab7e54351f70db6ac1ab83c679a2c042e7
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-09-24 23:08:45 +0530

    solving problem 20

--- Questions/Five.java
+++ Questions/Five.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 public class Five {
     public static void main(String[] args) {
+        // Find max and min in ArrayList
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(12,95,49,5,67,51,37,64,69,27,51,57,53,37,55,17));
 
         int min = list.get(0);
@@ -14,16 +15,27 @@ public class Five {
             if (min > integer) {
                 min = integer;
             }
-
             if (max < integer) {
                 max = integer;
             }
         }
 
-        System.out.println("Max value : "+max);
-        System.out.println("Min value : "+min);
+        System.out.println("Max value : " + max);
+        System.out.println("Min value : " + min);
+
+        // Convert array to ArrayList
+        int[] arr = {15, 48, 95, 35, 75};
+        
+        ArrayList<Integer> arrList = new ArrayList<>();
+        for (int num : arr) {
+            arrList.add(num);
+        }
+        System.out.println("ArrayList from array: " + arrList);
     }
 }
 
 // 18. **Find Maximum and Minimum Values**
 //     Find the max and min values in an `ArrayList` of integers.
+
+// 20. **Convert Array to ArrayList**
+//     Convert a regular array of integers into an `ArrayList`.
