f3344bc67fc2d29f8d4e85e90eaf9985953c8b7b
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-09-27 00:10:24 +0530

    solved problem 28,29 and 30

--- Questions/Seven.java
+++ Questions/Seven.java
@@ -43,6 +43,12 @@ public class Seven {
         ArrayList<Integer> list = new ArrayList<>();
         list.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
 
+        printEven(list);
+        printOdd(list);
+
+        System.out.println("Normal    : "+list);
+        System.out.println("Squared   : "+replaceSquare(list));
+
     }
 }
 
