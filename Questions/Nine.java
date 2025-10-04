734c125726ad9da702625b726c447b24e586cbbb
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-10-05 00:11:58 +0530

    finding common values between three lists

--- Questions/Nine.java
+++ Questions/Nine.java
@@ -2,6 +2,7 @@ package Questions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Nine {
 
@@ -11,18 +12,42 @@ public class Nine {
         return sum;
     }
 
+    public static ArrayList<Integer> commonEle(List<Integer> list1,List<Integer> list2,List<Integer> list3){
+        ArrayList<Integer> commonElements = new ArrayList<>();
+        for (Integer i : list1) {
+            for (Integer j : list2) {
+                for (Integer k : list3) {
+                    if ((i==j&&j==k) & !commonElements.contains(i)) {
+                        commonElements.add(i);
+                    }
+                }
+            }
+        }
+
+        return commonElements;
+    }
+
     //39 
-    public ArrayList<Integer> reverseList(ArrayList<Integer> list){
+    public static  List<Integer> reverseList(List<Integer> list){
         int n = list.size();
         for (int i = 0; i < n/2; i++) {
             int temp = list.get(i);
             list.set(i, list.get(n-1-i));
-            list.set(, null)
+            list.set((n-1-i), temp);
         }
+
+        return list;
     }
 
     public static void main(String[] args) {
         System.out.println("Sum : "+sum(new ArrayList<>(Arrays.asList(12,25,7,5,96,799,656,56,75))));
+
+        System.out.println("Original "+Arrays.asList(12,25,28,29,27));
+        System.out.println("Reveresed : "+reverseList(Arrays.asList(12,25,28,29,27)));
+
+        ArrayList<Integer> common = commonEle(Arrays.asList(15,67,9,695,3466,25), 
+        Arrays.asList(15,27,96,25,97,65),Arrays.asList(15,27,67,25));
+        System.out.println("Common between Three lists : "+common);
     }
 
 // 36. **Sum of All Elements**
