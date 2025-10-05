bfa1978627e07c60890a8f92c618b48f3798f014
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-10-05 23:51:00 +0530

    created countOdd method to count the number

--- Questions/Ten.java
+++ Questions/Ten.java
@@ -27,6 +27,7 @@ public class Ten {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
 
         System.out.println("Even Numbers : "+countEven(list));
+        System.out.println("Odd Numbers : "+countOdd(list));
     }
 }
 
