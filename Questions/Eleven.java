592e13e5ee61480add75ee6b8c9430c198cd0518
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-10-09 23:59:27 +0530

    Update Eleven.java

--- Questions/Eleven.java
+++ Questions/Eleven.java
@@ -4,3 +4,17 @@ public class Eleven {
     }
 }
 
+46. **Right Rotate by K Positions**
+    Rotate the `ArrayList` to the right by `k` positions.
+
+47. **Sum of Elements at Even Indices**
+    Find the sum of elements at even indices in an `ArrayList`.
+
+48. **Find Pairs with Given Sum**
+    Print all pairs of elements from an `ArrayList` whose sum is equal to a given number.
+
+49. **Find Triplets with Given Sum**
+    Find all triplets in the `ArrayList` that sum up to a given value.
+
+50. **Check if Two Lists are Disjoint**
+    Determine if two `ArrayList`s have no elements in common.
