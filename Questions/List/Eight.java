6843f3feb3900a3c3a42dd904031faaf62433787
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-10-11 23:49:46 +0530

    corrected package declaration

--- Questions/List/Eight.java
+++ Questions/List/Eight.java
@@ -1,10 +1,10 @@
-package Questions;
+package List;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collector;
+// import java.util.List;
+// import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Eight {
--- Questions/List/Nine.java
+++ Questions/List/Nine.java
@@ -1,4 +1,4 @@
-
+package List;
 
 import java.util.ArrayList;
 import java.util.Arrays;
