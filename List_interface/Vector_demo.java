23dd5de4a0c69e6e9685ed5be7f1261f8870bc92
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-10-12 12:31:22 +0530

    Create Vector_demo.java

---
+++ List_interface/Vector_demo.java
@@ -0,0 +1,16 @@
+public class Vector_demo {
+    public static void main(String[] args) {
+        
+    }
+
+    // vector was introduced in jdk 1 before the collection framewoks
+    // it is part of lagecy class later it was added into collections framework as implementation class of List interface.
+
+    // It is sychronized type of array which provides thread safety in multi-threaded environment.
+    // constructors : 
+    // 1. Vector() -> basic create vector with defacult capacity
+    // 2. Vector(int initalCapacity) -> basic create vector with given initial capacity
+    // 3. Vector(int initalCapacity, int capacityIncrement) -> basic create vector with given initial capacity, and capacityIncrement 
+    // 4. Vector(Collection<? extends E> c) -> creates the vector with passed collections like ArrayList and LinekdList.
+
+}
