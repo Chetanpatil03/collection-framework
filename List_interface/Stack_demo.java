fdd99a47209410663667d1b4f25e25e67533abd7
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-10-12 15:12:29 +0530

    usage of stack class

--- List_interface/Stack_demo.java
+++ List_interface/Stack_demo.java
@@ -1,6 +1,28 @@
+import java.util.Stack;
+
 public class Stack_demo {
     public static void main(String[] args) {
-        
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10); //top 1
+        stack.push(20); //top 2
+        stack.push(30); //top 3
+        stack.push(40); //top 4
+
+        System.out.println(stack);
+        System.out.println(stack.peek());
+        // System.out.println);
+        stack.pop();
+        System.out.println(stack);
+        System.out.println(stack.size());
+
+        // other methods that are present in Vector class in java
+        // -- add,remove,index,search, etc
+        stack.add(10);
+        System.out.println(stack);
+
+        // we can also implement this by LinkedList class
+        Link
+
     }
 
     // Stack is linear datastructure which extends Vector class in java
