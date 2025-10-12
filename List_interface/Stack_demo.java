3a25229b0a8e1c98d324ecdfd666ee4b62c9f48b
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-10-12 14:41:49 +0530

    Create Stack_demo.java

---
+++ List_interface/Stack_demo.java
@@ -0,0 +1,15 @@
+public class Stack_demo {
+    public static void main(String[] args) {
+        
+    }
+
+    // Stack is linear datastructure which extends Vector class in java
+    // Stack follows LIFO Last In First Out principle
+    // stack have methods like -> 
+    // 1. Push ->> Add element at top
+    // 2. Pop ->> Remove elemenet from top
+    // 3. Peek ->> Get element from top 
+    // 4. isEmpty ->> checks if stack is empty
+    // 5. size ->> return size of stack 
+    // etc all the methods of Vector class because it extends the vector class.
+}
