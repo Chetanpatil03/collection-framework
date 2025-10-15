126121af05b65aafb5f6aaec10366904246f1ec1
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-10-15 23:59:59 +0530

    update

---
+++ Map_interface/LinkedHashMap_demo.java
@@ -0,0 +1,14 @@
+package Map_interface;
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashMap_demo {
+
+    public static void main(String[] args) {
+        LinkedHashMap<String,Integer> fruits = new LinkedHashMap<>();
+
+        fruits.put("Apple", 30);
+        fruits.put("Mango", 30);
+        fruits.put("chery", 30);
+    }
+}
