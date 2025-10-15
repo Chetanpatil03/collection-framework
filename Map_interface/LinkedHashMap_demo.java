3920fb5be557100fd578285655f4d07785c085a4
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-10-16 00:06:37 +0530

    Update LinkedHashMap_demo.java

--- Map_interface/LinkedHashMap_demo.java
+++ Map_interface/LinkedHashMap_demo.java
@@ -1,14 +1,30 @@
 package Map_interface;
 
+import java.security.KeyStore.Entry;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class LinkedHashMap_demo {
 
     public static void main(String[] args) {
-        LinkedHashMap<String,Integer> fruits = new LinkedHashMap<>();
+        LinkedHashMap<String,Integer> fruits = new LinkedHashMap<>(10,0.8f,true);
 
         fruits.put("Apple", 30);
         fruits.put("Mango", 30);
-        fruits.put("chery", 30);
+        fruits.put("banana", 30);
+        fruits.put("tomato", 30);
+        fruits.put("guva", 30);
+
+        fruits.get("Apple");
+        fruits.get("banana");
+        fruits.get("Apple");
+        fruits.get("guva");
+        
+
+        for (Map.Entry<String,Integer> entry : fruits.entrySet()) {
+            System.out.println(entry.getKey() +" = "+entry.getValue()); //accessorder
+            // list recently used --> accessed order
+            // visited / accessed elements are in the last of next iteration of the list.
+        }
     }
 }
