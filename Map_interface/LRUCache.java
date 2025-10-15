9ec2804e9a9c7ab21d3fa4ec40192c763dc3c093
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-10-16 00:59:28 +0530

    removing least used element with given certain size

--- Map_interface/LRUCache.java
+++ Map_interface/LRUCache.java
@@ -34,5 +34,29 @@ public class LRUCache<K,V> extends LinkedHashMap<K,V>{
         cache.put("Prafulla", 80);
         System.out.println(cache);
 
+        // cache.get("Ram ");
+        // cache.put("Suresh", 90);
+        // System.out.println(cache);
+
+
+
     }
 }
+
+
+/**
+     * Demonstrates the usage of the LRUCache class with String keys and Integer values.
+     * <p>
+     * The main method performs the following actions:
+     * <ul>
+     *   <li>Creates an LRUCache instance with a maximum capacity of 3 entries.</li>
+     *   <li>Adds three key-value pairs to the cache.</li>
+     *   <li>Prints the current state of the cache.</li>
+     *   <li>Adds a fourth entry, causing the least recently used entry to be evicted.</li>
+     *   <li>Prints the updated state of the cache after eviction.</li>
+     *   <li>Contains commented-out code for further cache operations and printing.</li>
+     * </ul>
+     * This example illustrates how the LRU (Least Recently Used) eviction policy works in the cache.
+     *
+     * 
+     */
