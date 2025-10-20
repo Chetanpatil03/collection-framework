855864c675b66fe5045847f23bedc62545bc671c
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-10-20 22:42:17 +0530

    create firstRepeated() method to return the first repeated element

--- Questions/List/Ten.java
+++ Questions/List/Ten.java
@@ -5,47 +5,62 @@ import java.util.Arrays;
 
 public class Ten {
 
-    
-
-    public static int countEven(ArrayList<Integer> list){
+    // 43-1
+    public static int countEven(ArrayList<Integer> list) {
         int count = 0;
         for (Integer i : list) {
-            if (i % 2 ==0) {
+            if (i % 2 == 0) {
                 count++;
             }
         }
         return count;
     }
 
-    public static int countOdd(ArrayList<Integer> list){
+    // 43-2
+    public static int countOdd(ArrayList<Integer> list) {
         int count = 0;
         for (Integer i : list) {
-            if (i % 2 !=0) {
+            if (i % 2 != 0) {
                 count++;
             }
         }
         return count;
+    }
+
+    public static Integer firstRepeated(ArrayList<Integer> list) {
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).equals(list.get(j))) {
+                    return list.get(i);
+                }
+            }
+        }
+        return null;
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
+        ArrayList<Integer> list = new ArrayList<>(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
 
-        System.out.println("Even Numbers : "+countEven(list));
-        System.out.println("Odd Numbers : "+countOdd(list));
+        System.out.println("Even Numbers : " + countEven(list));
+        System.out.println("Odd Numbers : " + countOdd(list));
     }
 }
 
 // 41. **Find the First Repeating Element**
-//     Find the first element that appears more than once in an `ArrayList`.
+// Find the first element that appears more than once in an `ArrayList`.
 
 // 42. **Find the First Non-Repeating Element**
-//     Find the first element that appears only once in an `ArrayList`.
+// Find the first element that appears only once in an `ArrayList`.
 
 // 43. **Count Even and Odd Numbers**
-//     Count how many even and odd numbers are present in an `ArrayList`.
+// Count how many even and odd numbers are present in an `ArrayList`.
 
 // 44. **Move All Zeroes to End**
-//     Rearrange an `ArrayList` so that all `0`s are moved to the end, maintaining the order of non-zero elements.
+// Rearrange an `ArrayList` so that all `0`s are moved to the end, maintaining
+// the order of non-zero elements.
 
 // 45. **Left Rotate by One Position**
-//     Rotate the `ArrayList` to the left by one position (first element moves to end).
+// Rotate the `ArrayList` to the left by one position (first element moves to
+// end).
