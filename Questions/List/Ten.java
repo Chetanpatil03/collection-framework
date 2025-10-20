d869d3db22691077f0a60c1df51998507fef7f98
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-10-20 22:46:52 +0530

    Update Ten.java

--- Questions/List/Ten.java
+++ Questions/List/Ten.java
@@ -53,12 +53,14 @@ public class Ten {
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>(
-                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,21,20));
 
         System.out.println("Even Numbers : " + countEven(list));
         System.out.println("Odd Numbers : " + countOdd(list));
 
         System.out.println("First repeated element : "+firstRepeated(list));
+
+        System.out.println("First non repeated element : : "+firstNonRepeated(list));
     }
 }
 
