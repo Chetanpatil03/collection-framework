bcf7867862a02af96cbb940283b3f21820f6ffdb
Chetan Bachchhav <156205330+Chetanpatil03@users.noreply.github.com>
2025-10-26 23:03:01 +0530

    Add CharFrequency class to count character occurrences
    Implement a program to count character occurrences in a string using a HashMap.

---
+++ Questions/Map/CharFrequency.java
@@ -0,0 +1,19 @@
+Counting Character Occurrences in a String
+
+Classic use case for interviews.
+
+import java.util.*;
+
+public class CharFrequency {
+    public static void main(String[] args) {
+        String str = "programming";
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : str.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        System.out.println(map);
+        // Output: {p=1, r=2, o=1, g=2, a=1, m=2, i=1, n=1}
+    }
+}
