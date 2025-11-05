2cceb561523730c8e6e8b1a95260d4a8c116de76
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-11-05 19:57:19 +0530

    Update ComparableDemo.java

--- ComparableDemo.java
+++ ComparableDemo.java
@@ -1,16 +1,21 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ComparableDemo {
     public static void main(String[] args) {
-        List<Student> list = new ArrayList<>();
-        list.add(new Student("Chetan", 9.20));
-        list.add(new Student("Prafulla", 9.40));
-        list.add(new Student("Dipak", 8.40));
-        list.add(new Student("Nikhil", 9.20));
+        // List<Integer> list = new ArrayList<>();
+        // list.add(new Student("Chetan", 9.20));
+        // list.add(new Student("Prafulla", 9.40));
+        // list.add(new Student("Dipak", 8.40));
+        // list.add(new Student("Nikhil", 9.20));
 
-        // list.sort(null);  cause Student class does not have any natural sorting order
-        // so that why comaparable has been introduced for giving user defined class a natural sorting order.
+        List<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(15,26,68,69,12,22));
+
+        // list.sort(null); cause Student class does not have any natural sorting order
+        // so that why comaparable has been introduced for giving user defined class a
+        // natural sorting order.
 
         // Comparator --> Work on two object and used for custom sorting logic.
         // It has method compare(T o1, T o2) --> ex: compare(Student_1, Student_2)
@@ -19,24 +24,29 @@ public class ComparableDemo {
         // It has compareTo(T o1) method --> ex : Student_1.compareTo(Student_2)
 
         System.out.println(list);
+
+
+        Student student = new Student("chetan", 9.40);
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(student);
+
+        System.out.println(students);
     }
 }
 
-class Student implements Comparable<Student>{
+class Student implements Comparable<Student> {
     private String name;
     private double gpa;
-
 
     public Student(String name, double gpa) {
         this.name = name;
         this.gpa = gpa;
     }
 
-
     public String getName() {
         return name;
     }
-
 
     public double getGpa() {
         return gpa;
@@ -44,17 +54,11 @@ class Student implements Comparable<Student>{
 
     @Override
     public String toString() {
-        return "Student [Name : "+name+", GPA : "+gpa+" ]";
+        return "Student [Name : " + name + ", GPA : " + gpa + " ]";
     }
-
 
     @Override
     public int compareTo(Student o) {
-        return 1;
+        return Double.compare(o.getGpa(), this.getGpa());
     }
-
-
-
-    
-
 }
