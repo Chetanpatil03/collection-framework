4aa6ae0e4a6dc67ca3b036169e640399fd426332
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-11-07 01:01:48 +0530

    Update HashMapDemo.java

--- Map_interface/HashMapDemo.java
+++ Map_interface/HashMapDemo.java
@@ -1,5 +1,6 @@
 package Map_interface;
 
+import java.util.HashMap;
 import java.util.Hashtable;
 
 public class HashMapDemo {
@@ -15,6 +16,39 @@ public class HashMapDemo {
         System.out.println(hashtable.get(2));
         System.out.println(hashtable.containsKey(2));
         System.out.println(hashtable);
+
+        // Example : concurrency
+        HashMap<Integer,String> map = new HashMap<>();
+        Hashtable<Integer,String> hashtable_two = new Hashtable<>();
+ 
+        Thread thread = new Thread(()->{
+            for (int i = 0; i < 1000; i++) {
+                map.put(i, "Thread-1");
+                hashtable_two.put(i, "Thread-1");
+                
+            }
+        });
+        
+        Thread thread_2 = new Thread(()->{
+            for (int i = 0; i < 1000; i++) {
+                map.put(i, "Thread-2");
+                hashtable_two.put(i+1000, "Thread-2");
+            }
+        });
+
+        thread.start();
+        thread_2.start();
+
+        try{
+            thread.join();
+            thread_2.join();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Size of Map : "+map.size());
+        System.out.println("Size of HashTable : "+hashtable_two.size());
+        
 
     }
 }
