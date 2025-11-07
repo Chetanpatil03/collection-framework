f098ce42d7947c0ed8dedc5dc0361bf49e05fba3
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-11-07 23:51:04 +0530

    way to using Map.of() method to create immutable map

--- Map_interface/ImmutableMapDemo.java
+++ Map_interface/ImmutableMapDemo.java
@@ -17,8 +17,16 @@ public class ImmutableMapDemo {
         // using unmodifiableMap
         Map<Integer, String> unmodifiableMap = Collections.unmodifiableMap(map);
         //unmodifiableMap.put(4, "nikhil"); //throw UnsupportedOperationException exception
-
         System.out.println(unmodifiableMap);
+
+        // way --> 2 : using Map.of --> but it can only consist 10 key-value pair.
+
+        Map<Integer, String> immitubleMap = Map.of(1,"chetan",2,"ishwar",3,"navjit");
+        // was introduced in java 9
+        System.out.println(immitubleMap);
+        
+        
+
 
     }
 }
