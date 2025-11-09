00fc7075d6ebce613bb4354a9ddf298d1fe57daa
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-11-09 14:54:38 +0530

    Create demo.java

---
+++ Java_8/demo.java
@@ -0,0 +1,11 @@
+package Java_8;
+
+public class demo {
+    public static void main(String[] args) {
+        int x = 2;
+
+        if (x/0 == 0 || x > 0 ) System.out.println("success");
+        else System.out.println("Failed");
+
+    }
+}
