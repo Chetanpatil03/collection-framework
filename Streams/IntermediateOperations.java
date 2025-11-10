24c2699125de1ddfae6f1d3e6d783af02b6b68cf
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-11-10 14:30:53 +0530

    terminal operations --> map,filter, sorted, distinct

--- Streams/IntermediateOperations.java
+++ Streams/IntermediateOperations.java
@@ -1,6 +1,7 @@
 package Streams;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class IntermediateOperations {
@@ -15,7 +16,7 @@ public class IntermediateOperations {
         // takes Predicate as parameter
         // example : 
         
-        List<String> list = Arrays.asList("Chetan","Prafulla","Dipak","Nikhil");
+        List<String> list = Arrays.asList("Chetan","Prafulla","Dipak","Chetan","Prafulla","Nikhil");
         Stream<String> stream = list.stream().filter(x -> x.startsWith("C"));
         // not filterd cause no terminal operations.
 
@@ -24,5 +25,37 @@ public class IntermediateOperations {
         // equivalent to --> 
         // list.stream().filter(x-> x.startsWith("C")).count();
         System.out.println("Res :"+res);
+
+        // 2. Map operation : 
+        List<String> collect = list.stream().map(String::toUpperCase).collect(Collectors.toList());
+        System.out.println("Original : "+ list);
+        System.out.println("Mapped   : "+collect);
+
+        // 3. Sorted 
+        // used to sort the collection in natural as well as custome manner
+        // take Comparator as input
+
+        Stream<String> sorted = list.stream().sorted();
+        Stream<String> sortedWithComparator = list.stream().sorted((a,b) -> a.length() - b.length());
+
+        System.out.println(sorted.collect(Collectors.toList()));
+        System.out.println(sortedWithComparator.collect(Collectors.toList()));
+
+        // 4. Distinct
+        // return stream of unique elements
+        // does not take any 
+        // can be applied after filter
+
+        List<String> collect2 = list.stream().distinct().collect(Collectors.toList());
+        Long distinctWithFilter = list.stream()
+        .filter(x-> x.toLowerCase().startsWith("c"))
+        .distinct()
+        .count();
+
+        System.out.println("Distinct elements : "+collect2);
+        System.out.println("Count : "+distinctWithFilter);
+
+        
+    
     }
 }
