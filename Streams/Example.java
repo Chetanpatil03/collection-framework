7f02b3d2fd20941dc62755a74ec7ba23e1eab00b
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-11-10 15:50:50 +0530

    solving examples

--- Streams/Example.java
+++ Streams/Example.java
@@ -10,6 +10,22 @@ public class Example {
         List<String> queList = Arrays.asList("Chetan","Patil","Ram","Shyam","August");
         System.out.println(queList.stream().filter(x-> x.length() > 3).toList());
 
+        // 2 : square and sorting
+
+        List<Integer> squareSort = Arrays.asList(5,2,9,1,6);
+        squareSort.stream()
+        .map(x-> x*x)
+        .sorted()
+        .toList()
+        .forEach(x-> System.out.print(x+" "));
+
+        // 3 : find sum
+        List<Integer> integers = Arrays.asList(1,5,17,59,37,59);
+        System.out.println("\nSum : "+
+            integers.stream().reduce(1, Integer::sum)
+        );
+
         
+
     }
 }
