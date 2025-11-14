ec3d912360ec3a59f1392a23725d5d2ee08d5d54
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-11-14 23:14:09 +0530

    Create RemoveDuplicates.java

---
+++ Problems/RemoveDuplicates.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int [] arr = {15,6,8,6,9,6,58,69,68,94,27,31,89,46};
+        System.out.print("Original :: ");
+        Arrays.stream(arr).forEach(x -> System.out.print(x+" "));
+        System.out.println();
+
+
+        
+
+
+    }
+}
