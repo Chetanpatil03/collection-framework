f61e9b04ffac64f04f17a5539f0da272ada707ef
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-11-15 23:48:25 +0530

    Update Pallindrom.java

--- Problems/Pallindrom.java
+++ Problems/Pallindrom.java
@@ -2,17 +2,29 @@ import java.util.Scanner;
 
 public class Pallindrom {
 
-    public static boolean isPallindrom(String string){
+    public static boolean isPallindromString(String string){
         return string.equals(new StringBuffer(string).reverse().toString());
     }
-    public static void main(String[] args) {
+
+    public static String inpString(Scanner sc){
         System.out.print("Enter String :: ");
+        return sc.nextLine();
+    }
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String inp = sc.nextLine();
+        System.out.print("What do you want to do[1.INT 2.String 3.EXIT] :: ");
+        int ch = sc.nextInt();
+        if (ch == 2) {
+            String string = inpString(sc);
+            System.out.print("The string [ " +string+ " ] is :: ");
+            System.out.println(isPallindromString(string) ?"Pallindrom" :"Not a pallindrom");
+        }
+        else if (ch == 1) {
+            
+        }
 
-        System.out.print("The string [ " +inp+ " ] is :: ");
-        System.out.println(isPallindrom(inp) ?"Pallindrom" :"Not a pallindrom");
         sc.close();
 
     }
