222384b243d2b87dffbc6f06288cae98b0918e33
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-11-16 23:42:08 +0530

    solved question number five

--- Problems/FirstTen.java
+++ Problems/FirstTen.java
@@ -46,6 +46,13 @@ public class FirstTen {
         return a > b ?( a > c ? a : c) : b > c ? b : c; 
     }
 
+    // five
+    public static void multiplicationTable(int n){
+        for(int i = 1; i<=10;i++){
+            System.out.println(n+" * "+i+" = "+ n*i + "");
+        }
+    }
+
 
 
     public static void main(String[] args) {
