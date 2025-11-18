0cf9d4a4b481b14fbaa6956bcec441fdd04713f2
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-11-18 18:38:23 +0530

    implementing selection sort technique for optimized sorting

--- Problems/SecondTen.java
+++ Problems/SecondTen.java
@@ -35,6 +35,7 @@ public class SecondTen {
         return arr;
     }
 
+    // bubble sort
     public static int [] sortArray_B(int []arr){
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
@@ -47,6 +48,26 @@ public class SecondTen {
         }
 
         return arr;
+    }
+    // Method to perform selection sort
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element of the unsorted part
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
     }
 
     public static void main(String[] args) {
