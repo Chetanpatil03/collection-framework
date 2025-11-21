e8f6e79685b004e2a47ac794a2ae0040f3d25a94
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-11-21 22:36:04 +0530

    implemented function that prints all prime number between 1 - 100

--- Problems/ThirdTen.java
+++ Problems/ThirdTen.java
@@ -18,8 +18,8 @@ public class ThirdTen {
     // finding is prime
     public static boolean isPrime(int n){
         if (n <= 1) return false;
-        for (int i = 2; i < Math.sqrt(n); i++) {
-            if (n % 2 == 0) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
         }
 
         return true;
@@ -35,7 +35,7 @@ public class ThirdTen {
     public static void main(String[] args) {
         System.out.println("Year [1996] :: "+isLeap(1996));
         arrayAverage(new int[]{15,4,95,67,95,36,76,24,38,62});
-        
+        printingPrime();
     }
 }
 
