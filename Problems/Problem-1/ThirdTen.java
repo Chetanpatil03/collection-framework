dc20f741eddf68d5e79d11312564d6105b14dc5d
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-11-22 19:12:09 +0530

    updated repository structure

---
+++ Problems/Problem-1/ThirdTen.java
@@ -0,0 +1,53 @@
+public class ThirdTen {
+
+    // year is leap or not
+    public static boolean isLeap(int year){
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0 )? true : false;
+    }
+
+    // average of array elements
+    public static void arrayAverage(int [] arr){
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+
+        System.out.println("Average of array :: "+sum/arr.length);
+    }
+
+    // finding is prime
+    public static boolean isPrime(int n){
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    public static void printingPrime(){
+        for (int i = 1; i < 100; i++) {
+            if (isPrime(i)) System.out.println(i);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Year [1996] :: "+isLeap(1996));
+        arrayAverage(new int[]{15,4,95,67,95,36,76,24,38,62});
+        printingPrime();
+    }
+}
+
+/* Question to solve
+21. Write a program to find the second largest element in an array.
+22. Write a program to check if two strings are anagrams.
+23. Write a program to print all prime numbers between 1 and 100.
+24. Write a program to find the sum of digits of a number.
+-- 25. Write a program to check if a year is a leap year.
+26. Write a program to find the average of elements in an array.
+27. Write a program to remove duplicate elements from an array.
+28. Write a program to convert a decimal number to binary.
+29. Write a program to find the frequency of each character in a string.
+30. Write a program to merge two arrays.
+ */
---
+++ Problems/Random/Pallindrom.java
@@ -0,0 +1,39 @@
+package Random;
+import java.util.Scanner;
+
+public class Pallindrom {
+
+    public static boolean isPallindromString(String string){
+        return string.equals(new StringBuffer(string).reverse().toString());
+    }
+
+    public static String inpString(Scanner sc){
+        System.out.print("Enter String :: ");
+        return sc.nextLine();
+    }
+
+    public int revNum(int n){
+        int rev = n;
+        while (n > 0) {
+            rev += 
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What do you want to do[1.INT 2.String 3.EXIT] :: ");
+        int ch = sc.nextInt();
+        if (ch == 2) {
+            String string = inpString(sc);
+            System.out.print("The string [ " +string+ " ] is :: ");
+            System.out.println(isPallindromString(string) ?"Pallindrom" :"Not a pallindrom");
+        }
+        else if (ch == 1) {
+            
+        }
+
+        sc.close();
+
+    }
+}
