cf22a65468ee97cd7c87ada1b2205d124cfa128c
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-06 00:00:11 +0530

    Update ArraysRelated.java

--- Problems/Problem-2/ArraysRelated.java
+++ Problems/Problem-2/ArraysRelated.java
@@ -90,5 +90,15 @@ public class ArraysRelated {
 7. Rotate an array (left/right).
 -- 8. Merge two sorted arrays into a single sorted array.
 9. Check if two arrays are equal.
+
+
+
+coinadoinzx
+aopvhpn
+sfeibs
+
+tiob
+
+asvasb
 10. Find pairs in an array whose sum is equal to a given number.
  */
