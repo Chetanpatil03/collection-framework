f394dc2068abe033e1c9366833497acfa9023882
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-16 23:55:28 +0530

    solved the first question in which printing sequencing 1 to 100 without using loops

---
+++ Problems/Problem-3/FirstTen.java
@@ -0,0 +1,32 @@
+
+public class FirstTen {
+    // Print numbers from 1 to 100 without using loops.
+    public static void printNum(int n){
+        if (n <= 100) {
+            System.out.print(n+" ");
+            printNum(n+1);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Sequence ::  ");
+        printNum(1);
+    }
+}
+
+/*
+### **Basic Program Logic**
+
+-- 1. Print numbers from 1 to 100 without using loops.
+2. Find the average of N numbers.
+3. Convert Celsius to Fahrenheit.
+4. Check if a character is a vowel, consonant, digit, or special character.
+5. Count positive, negative, and zero values from an input list.
+6. Find the largest and smallest digit in a number.
+7. Convert lowercase string to uppercase (without using built-in).
+8. Print multiplication table of a number.
+9. Calculate power of a number without using Math.pow().
+10. Print all factors of a number.
+ */
+
+
+// ctrl + shift + p --> clean java language server workplace
