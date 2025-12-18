106195f653a8400a6d74e3069b8b621904b82998
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-18 17:44:12 +0530

    Create Three.java

---
+++ Problems/Problem-3/Three.java
@@ -0,0 +1,13 @@
+public class Three {
+    public static void main(String[] args) {
+        
+    }
+}
+
+/*
+16. Count even and odd numbers in an array.
+17. Find the number of elements greater than a given value.
+18. Calculate sum + average of array elements.
+19. Create a new array containing only even numbers.
+20. Multiply all elements of an array.
+ */
