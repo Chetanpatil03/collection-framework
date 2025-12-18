673f3a20616fc436b3b4ab72aa5d11f3c09019ea
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-18 17:48:36 +0530

    solved question count even and odd numbers in an array.

--- Problems/Problem-3/Three.java
+++ Problems/Problem-3/Three.java
@@ -1,6 +1,20 @@
+import java.util.Map;
+
 public class Three {
+    // Count even and odd numbers in an array.
+    public static Map<String,Integer> countEvenODD(int [] arr){
+        int evenCount = 0, oddCount =0;
+
+        for (int i : arr) {
+            if (i % 2 == 0) evenCount++;
+            else oddCount++;
+        }
+
+        return Map.of("Even :",evenCount,"Odd :",oddCount);
+    }
     public static void main(String[] args) {
-        
+        int []arr = {12,59,4,59,14,9,841,8,9,51,48,54,18,5,4,19,685,41};
+        System.out.println("Even and Odd Count :: "+countEvenODD(arr));
     }
 }
 
