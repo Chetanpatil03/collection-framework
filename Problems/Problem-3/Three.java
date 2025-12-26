b6dec4135d3ffe0d6a3a8003e34f83fe4459352e
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-26 14:26:16 +0530

    solved question number 19 which is creating a even array from given array

--- Problems/Problem-3/Three.java
+++ Problems/Problem-3/Three.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Map;
 
 public class Three {
@@ -16,6 +17,12 @@ public class Three {
         int []arr = {12,59,4,59,14,9,841,8,9,51,48,54,18,5,4,19,685,41};
         System.out.println("Even and Odd Count :: "+countEvenODD(arr));
         System.out.println("Summ and Average :: "+calculateSumAvg(arr));
+        System.out.print("Only evens : ");
+        int [] newEven = onlyEvens(arr);
+        for (int i : newEven) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
     }
 
     public static Map<String,Integer> calculateSumAvg(int []arr){
@@ -25,12 +32,16 @@ public class Three {
 
         return Map.of("Average :",avg, "Sum :",sum);
     }
+
+    public static int[] onlyEvens(int []arr){
+            return Arrays.stream(arr).filter(x -> x%2 == 0).toArray();
+    }
 }
 
 /*
 -- 16. Count even and odd numbers in an array.
 -- 17. Find the number of elements greater than a given value.
 -- 18. Calculate sum + average of array elements.
-19. Create a new array containing only even numbers.
+-- 19. Create a new array containing only even numbers.
 20. Multiply all elements of an array.
  */
