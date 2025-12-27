d50dc6c19e00fe493e060024921130eea561aee4
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-27 15:36:29 +0530

    solved question number one using streams

--- 1_interview/One.java
+++ 1_interview/One.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
+
 public class One {
     public static void main(String[] args) {
 
         int [] arr = {1,2,3,4,5,6};
         int sum = sum(arr);
         System.out.println("Sum of array Elements :: "+sum);
+        System.out.println("Sum of array Elements :: "+sumInteger(arr));
     }
 
     public static int sum(int []arr){
@@ -13,4 +16,10 @@ public class One {
         }
         return sum;
     }
+
+    public static Integer sumInteger(int []arr){
+        return Arrays.stream(arr).sum(); 
+    }
+
+
 }
---
+++ 1_interview/Two.java
@@ -0,0 +1,12 @@
+public class Two {
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void max(
+
+
+    ){
+
+    }
+}
