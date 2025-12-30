7c9bfd5b6ad584cb4ab5a0a87925f65c8cf9df30
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-31 00:09:43 +0530

    Update Pallindrom.java

--- Problems/Random/Pallindrom.java
+++ Problems/Random/Pallindrom.java
@@ -12,28 +12,33 @@ public class Pallindrom {
         return sc.nextLine();
     }
 
-    public int revNum(int n){
-        int rev = n;
+    public static int revNum(int n){
+        int rev = 0;
         while (n > 0) {
-            rev += 
+            rev = rev * 10 + (n % 10);
+            n = n / 10;
         }
+
+        return rev;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("What do you want to do[1.INT 2.String 3.EXIT] :: ");
-        int ch = sc.nextInt();
-        if (ch == 2) {
-            String string = inpString(sc);
-            System.out.print("The string [ " +string+ " ] is :: ");
-            System.out.println(isPallindromString(string) ?"Pallindrom" :"Not a pallindrom");
-        }
-        else if (ch == 1) {
+        // System.out.print("What do you want to do[1.INT 2.String 3.EXIT] :: ");
+        // int ch = sc.nextInt();
+        // if (ch == 2) {
+        //     String string = inpString(sc);
+        //     System.out.print("The string [ " +string+ " ] is :: ");
+        //     System.out.println(isPallindromString(string) ?"Pallindrom" :"Not a pallindrom");
+        // }
+        // else if (ch == 1) {
             
-        }
+        // }
 
-        sc.close();
+        // sc.close();
+
+        System.out.println("reverse numbers [12354] :: "+revNum(12354));
 
     }
 }
