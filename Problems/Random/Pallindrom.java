c5a5adf0a114e7a0fb62039b63f73cc316b4efa2
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-31 00:14:59 +0530

    menu driven pallindrom check

--- Problems/Random/Pallindrom.java
+++ Problems/Random/Pallindrom.java
@@ -9,6 +9,7 @@ public class Pallindrom {
 
     public static String inpString(Scanner sc){
         System.out.print("Enter String :: ");
+        sc.nextLine();
         return sc.nextLine();
     }
 
@@ -22,23 +23,29 @@ public class Pallindrom {
         return rev;
     }
 
+    public static boolean isNumPallindrom(int number){
+        return number == revNum(number);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // System.out.print("What do you want to do[1.INT 2.String 3.EXIT] :: ");
-        // int ch = sc.nextInt();
-        // if (ch == 2) {
-        //     String string = inpString(sc);
-        //     System.out.print("The string [ " +string+ " ] is :: ");
-        //     System.out.println(isPallindromString(string) ?"Pallindrom" :"Not a pallindrom");
-        // }
-        // else if (ch == 1) {
-            
-        // }
+        System.out.print("What do you want to do[1.INT 2.String 3.EXIT] :: ");
+        int ch = sc.nextInt();
+        if (ch == 2) {
+            String string = inpString(sc);
+            System.out.print("The string [ " +string+ " ] is :: ");
+            System.out.println(isPallindromString(string) ?"Pallindrom" :"Not a pallindrom");
+        }
+        else if (ch == 1) {
+            System.out.print("Enter number :: ");
+            int number = sc.nextInt();
+            System.out.print("The string [ " +number+ " ] is :: ");
+            System.out.println(isNumPallindrom(number)?"Pallindrom" :"Not a pallindrom");
+        }
+        sc.close();
 
-        // sc.close();
-
-        System.out.println("reverse numbers [12354] :: "+revNum(12354));
+        // System.out.println("reverse numbers [12354] :: "+revNum(12354));
 
     }
 }
