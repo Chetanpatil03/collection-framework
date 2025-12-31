b228cc544b9764d1262bbf55303bce2fa0fbeb54
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-31 23:40:50 +0530

    created isPallindromAfterChanges() and reverseString() methods to check if string is pallindrom or not after removing digits

--- Problems/Problem-3/Strings.java
+++ Problems/Problem-3/Strings.java
@@ -18,13 +18,33 @@ public class Strings {
         System.out.println("Substring :: "+mySubString(name, 7, 15));
         System.out.println("Substring :: "+mySubString(name, 16, name.length()));
 
+
+        String check = "nayan123";
+        System.out.println("Is string Pallindrom [ "+check+" ] :: "+isPallindromAfterChanges(check));
+    }
+
+    public static boolean isPallindromAfterChanges(String string){
+        char [] arr = string.toCharArray();
+        String updated = "";
+
+        for (char c : arr) {
+            if (Character.isLetter(c)) {
+                updated += c;
+            }
+        }
+    
+        return updated.equals(reverseString(updated));
+    }
+
+    public static String reverseString(String string){
+        return new StringBuffer(string).reverse().toString();
     }
     
 }
 
 /*
 -- 6. Implement your own `substring()` method.
-7. Check if a string is a valid palindrome after removing non-alphanumeric characters.
+-- 7. Check if a string is a valid palindrome after removing non-alphanumeric characters.
 8. Longest palindrome substring (medium difficulty).
 9. Check if two strings are isomorphic.
 10. Compress a string (e.g., aaabb → a3b2).
