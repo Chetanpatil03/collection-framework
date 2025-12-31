25db2ebdad78d630fdbf8de4fe03fd6af9f8a48b
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-31 23:54:14 +0530

    Create Four.java

---
+++ Problems/Problem-3/Four.java
@@ -0,0 +1,15 @@
+public class Four {
+    public static void main(String[] args) {
+                
+    }
+}
+
+/*
+### **Arrays (Intermediate)**
+
+-- 1. Remove all occurrences of a given element from an array.
+2. Find the longest increasing subarray.
+3. Find the leader elements in an array (elements greater than all to their right).
+4. Move all zeros to the end of the array.
+5. Sum of all subarrays.
+ */
