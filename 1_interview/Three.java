24932f6d31d8fab9142085b38c3041496d20f257
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-04 01:29:11 +0530

    Create Three.java

---
+++ 1_interview/Three.java
@@ -0,0 +1,14 @@
+
+
+class Three {
+    public static void main(String[] args) {
+        
+		System.out.println("+---------+----------------------+--------+----------+-------------------+");
+		System.out.println("| book_id | title                | year   | quantity | author            |");
+		System.out.println("+---------+----------------------+--------+----------+-------------------+");
+		
+		System.out.printf("| %-7d | %-20s | %-6d | %-8d | %-15s   |\n",1,"Chetan rav",2024,15,"Prakash");
+		System.out.println("+---------+----------------------+--------+----------+-------------------+");
+        
+    }
+}
