aed3e16affe65d9205853a9f9cb64d19e7cd60d4
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-04 14:20:24 +0530

    Update Three.java

--- 1_interview/Three.java
+++ 1_interview/Three.java
@@ -1,14 +1,49 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Three {
-    public static void main(String[] args) {
-        
+	public static void printBooks(){
 		System.out.println("+---------+----------------------+--------+----------+-------------------+");
 		System.out.println("| book_id | title                | year   | quantity | author            |");
 		System.out.println("+---------+----------------------+--------+----------+-------------------+");
 		
 		System.out.printf("| %-7d | %-20s | %-6d | %-8d | %-15s   |\n",1,"Chetan rav",2024,15,"Prakash");
 		System.out.println("+---------+----------------------+--------+----------+-------------------+");
+	}
+	
+	public static void printUser() {
+		System.out.println("+---------+--------------------+--------------------+---------------+----------+");
+		System.out.println("| user_id | name               | email              | phone         | status   |");
+		System.out.println("+---------+--------------------+--------------------+---------------+----------+");
+		System.out.printf("| %-7d | %-18s | %-18s | %-13s | %-8s |\n",1,"Chetan rav","chetan@email","9284691048","ACTIVE");
+		System.out.println("+---------+--------------------+--------------------+---------------+----------+");
+	}
+    public static void main(String[] args) {
+        printUser();
+		ArrayList<String> list = new ArrayList<>(Arrays.asList("Java","Pyton","Springboot"));
+		currentBorrowedBooks(list);
+		
         
     }
+
+	public static void currentBorrowedBooks(ArrayList<String> books) {
+		 
+		
+		if (!books.isEmpty()) {
+			System.out.println("Books borrowed by user :: "+"Chetan"+" [ "+102+"]");
+			System.out.println("+---------+--------------------+");
+			System.out.println("| Sr.No.  | Book name          |");
+			System.out.println("+---------+--------------------+");
+			
+			for (int i = 0; i < books.size(); i++) {
+				System.out.printf("| %-7d | %-18s |\n",i+1, books.get(i));
+			}
+			System.out.println("+---------+--------------------+");
+			
+			return;
+		}
+		
+		System.out.println("There are no books borrowed by user :: "+"Chetan"+" [ "+102+"]");
+	}
 }
