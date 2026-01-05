bd84b0c42dba0b0d52434bbfbace2ff13dac320b
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-05 23:42:05 +0530

    solved question finding largest element in the array

--- 1_interview/Two.java
+++ 1_interview/Two.java
@@ -1,12 +1,23 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Two {
     public static void main(String[] args) {
-        
+        int[] arr = {12,52,68,95,6,86,9,6,76,54,25,85,68};
+        max(arr);
+
+        int max = Arrays.stream(arr).max().getAsInt();
+        System.out.println("Max : "+max);
     }
     
-    public static void max(
+    public static void max(int [] arr){
+        int max = arr[0];
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
+            }
+        }
 
-
-    ){
-
+        System.out.println("Largest element :: "+max);
     }
 }
