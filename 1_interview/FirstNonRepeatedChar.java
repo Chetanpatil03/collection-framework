cdf69075bc250c84a7e6513535cd4446f5383821
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-07 20:06:55 +0530

    solved question finding first non repeating character in String

---
+++ 1_interview/FirstNonRepeatedChar.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstNonRepeatedChar {
+    public static void main(String[] args) {
+        String string = "swiss";
+        System.out.println("First Non Repeating Character :: "+FirstNonRepeatingChar(string));
+        // System.out.println("First Non Repeating Character :: "+firstNonRepeatedChar(string));
+    }
+
+    public static char FirstNonRepeatingChar(String str){
+        char [] charArray = str.toCharArray();
+        // System.out.println("Array :: "+charArray);
+
+        Map<Character, Integer> count = new LinkedHashMap<>();
+
+        for (char c : charArray) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        for (HashMap.Entry<Character, Integer> entry : count.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+
+        return '\0';
+    }
+}
