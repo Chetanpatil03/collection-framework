0b466742bfa45bd67c24448495221e10895d9cc6
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-09 23:59:43 +0530

    Update Duplicates.java

--- 1_interview/Duplicates.java
+++ 1_interview/Duplicates.java
@@ -5,18 +5,21 @@ import java.util.Map;
 
 public class Duplicates {
     public static void main(String[] args) {
-        
+        i 
     }
 
-    public static void duplicateElements(int [] arr){
+    public static List<Integer> duplicateElements(int [] arr){
         HashMap<Integer,Integer> hashMap = new HashMap<>();
 
         for (int i : arr) hashMap.put(i, hashMap.getOrDefault(i, 0)+1);
 
         List<Integer> list = new ArrayList<>();
         for (Map.Entry<Integer,Integer> entry : hashMap.entrySet()) {
-            
+            if (entry.getValue() >= 2) {
+                list.add(entry.getKey());
+            }
         }
 
+        return list;
     }
 }
