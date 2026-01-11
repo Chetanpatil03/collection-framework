5f3fd8f1804f0ebb679da54c575764ee11d29055
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-11 23:54:56 +0530

    finding frequency of characters in String

--- 1_interview/CountingFrequency.java
+++ 1_interview/CountingFrequency.java
@@ -5,9 +5,11 @@ import java.util.Map;
 
 public class CountingFrequency {
     public static void main(String[] args) {
-        int [] arr = {1,5,1,2,5,9,14,8,58,2,5,8,75,9,5,5,9,2};
-        System.out.println("Frequency of elements :: "+countFrequency(arr));
-        System.out.println("Frequency of elements :: "+countFrequencySorted(arr));
+        // int [] arr = {1,5,1,2,5,9,14,8,58,2,5,8,75,9,5,5,9,2};
+        // System.out.println("Frequency of elements :: "+countFrequency(arr));
+        // System.out.println("Frequency of elements :: "+countFrequencySorted(arr));
+
+        System.out.println("Frequency :: "+countCharacterFrequency("Chetan bachchhav "));
     }
 
     public static Map countFrequency(int []arr){
@@ -20,6 +22,16 @@ public class CountingFrequency {
         Arrays.sort(arr);
         Map<Integer, Integer> map = new LinkedHashMap<>();
         for (int i : arr) map.put(i, map.getOrDefault(i, 0)+1);
+        return map;
+    }
+
+    public static Map countCharacterFrequency(String str){
+        char [] arr = str.trim().toCharArray();
+        Arrays.sort(arr);
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : arr) map.put(c, map.getOrDefault(c, 0)+1);
+
         return map;
     }
 
