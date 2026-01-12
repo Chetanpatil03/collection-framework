18ef0361690f9410ebe2bc43d4cd39bd64961f65
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-12 10:31:47 +0530

    solved problem finding kth largest number in an array

--- 1_interview/KthLargestNumber.java
+++ 1_interview/KthLargestNumber.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class KthLargestNumber {
     public static void main(String[] args) {
-        
+        int [] arr = {15,9,6,7,8,5,9,64,25,869,94,6,45};
+        Arrays.sort(arr);
+        Arrays.stream(arr).forEach(x -> System.out.print(x+" "));
+        System.out.println("\nFinding "+2+"th largest number :: "+kthLargestNumber(arr, 2));
     }
 
-    public int kthLargestNumber(){
-        
+    public static int kthLargestNumber(int[] arr, int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i : arr) {
+            pq.offer(i);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        return pq.peek();
     }
 }
