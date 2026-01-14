6c7883b352cb44bc65d8abe0b05d555bd350321a
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-14 13:04:11 +0530

    Update ReverseString.java

--- 1_interview/ReverseString.java
+++ 1_interview/ReverseString.java
@@ -5,6 +5,8 @@ public class ReverseString {
         System.out.println("Reverse :: "+revString(str));
         System.out.println("Reverse :: "+reverse(str));
         System.out.println("Reverse :: "+revString(str.toCharArray()));
+        
+        System.out.println("Reversed word [ Hello my name is chetan ]:: "+reverseWords("Hello my name is chetan"));
     }
 
     public static String revString(String string){
@@ -31,5 +33,16 @@ public class ReverseString {
         }
 
         return new String(arr);
+    }
+
+    public static String reverseWords(String string){
+        String [] arr = string.split("\\s");
+        String revString = "";
+        int n = arr.length;
+        for (int i = n - 1; i >= 0; i--) {
+            revString += arr[i] + " ";
+        }
+
+        return revString;
     }
 }
