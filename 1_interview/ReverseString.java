b22ab7e6adcfd574238af793105fa861f7e6d36a
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-14 12:39:53 +0530

    Create ReverseString.java

---
+++ 1_interview/ReverseString.java
@@ -0,0 +1,35 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello world";
+
+        System.out.println("Reverse :: "+revString(str));
+        System.out.println("Reverse :: "+reverse(str));
+        System.out.println("Reverse :: "+revString(str.toCharArray()));
+    }
+
+    public static String revString(String string){
+        String rev = "";
+
+        for (int i = string.length() - 1; i >= 0; i--) {
+            rev += string.charAt(i);
+        }
+        return rev;
+    }
+
+    public static String reverse(String str){
+        return new StringBuffer(str).reverse().toString();
+    }
+
+    public static String revString(char [] arr){
+
+        int n = arr.length;
+
+        for (int i = 0; i < n / 2; i++) {
+            char temp = arr[i];
+            arr[i] = arr[n-1-i];
+            arr[n-1-i] = temp;
+        }
+
+        return new String(arr);
+    }
+}
