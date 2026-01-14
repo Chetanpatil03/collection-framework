1b005508738f6bb043d3d581dcae57b3ceecd0c2
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-14 12:10:57 +0530

    Create SecondLarge.java

---
+++ 1_interview/SecondLarge.java
@@ -0,0 +1,25 @@
+public class SecondLarge {
+    public static void main(String[] args) {
+
+        int [] arr = {25,69,95,5,9,6,6,85,9,6,5,9,5,5,6,56};
+        System.out.println("Second large element :: "+secondLarge(arr));        
+    }
+
+    public static int secondLarge(int [] arr){
+        int max = Integer.MIN_VALUE;
+        int secMax = Integer.MIN_VALUE;
+
+        for (int i : arr) {
+            if (i > max) {
+                secMax = max;
+                max = i;
+            }
+            else if ( i > secMax && i != max) {
+                secMax = i;
+            }
+        }
+
+        return secMax;
+
+    }
+}
