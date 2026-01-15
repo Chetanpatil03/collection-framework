7f00b0d0e4b71d9e232028fa536fad0ef8069b36
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-16 00:59:56 +0530

    count digits in number and reverse number without using any inbuilt function

--- 1_interview/Pallindrom.java
+++ 1_interview/Pallindrom.java
@@ -2,6 +2,9 @@ public class Pallindrom {
     public static void main(String[] args) {
         System.out.println("Is pallindrom :: "+isPallindrom(121));
         System.out.println("Is pallindrom :: "+isPallindrom(12));
+
+        countDigits(121);
+        reverseNumber(12365);
     }
 
     public static boolean isPallindrom(int num){
@@ -13,5 +16,25 @@ public class Pallindrom {
         }
 
         return rev == og;
+    }
+
+    public static void countDigits(int digit){
+        int count = 0;
+        while (digit != 0) {
+            digit = digit / 10;
+            count++;
+        }
+
+        System.out.println("Count :: "+count);
+    }
+
+    public static void reverseNumber(int number){
+        int rev = 0;
+
+        while (number != 0) {
+            rev = rev * 10 + (number % 10);
+            number /= 10;
+        }
+        System.out.println("Reverse number :: "+rev);
     }
 }
