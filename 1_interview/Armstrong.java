ebfa3fe7665c86546bc1d3b8716650ccdabe156c
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-18 23:31:53 +0530

    Update Armstrong.java

--- 1_interview/Armstrong.java
+++ 1_interview/Armstrong.java
@@ -2,4 +2,12 @@ public class Armstrong {
     public static void main(String[] args) {
         
     }
+
+    public static int cube(int n){
+        return n*n*n;
+    }
+
+    public static void isArmstrong(int num){
+
+    }
 }
