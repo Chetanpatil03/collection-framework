fecf9f8872fcfa4596e263ebcff5e6e3072cf3dc
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-18 23:44:18 +0530

    Implemented logic for armstrong number

--- 1_interview/Armstrong.java
+++ 1_interview/Armstrong.java
@@ -1,13 +1,21 @@
 public class Armstrong {
     public static void main(String[] args) {
-        
+        System.out.println("Number is Armstrong :: "+isArmstrong(153));
     }
 
     public static int cube(int n){
         return n*n*n;
     }
 
-    public static void isArmstrong(int num){
+    public static boolean isArmstrong(int num){
+        int og = num;
+        int sum = 0;
+        while (num != 0) {
+            int rem = num % 10;
+            sum += cube(rem);
+            num /= 10;
+        }
 
+        return og == sum;
     }
 }
