6d411fa5e6aad538f1bbdf19196a6530b486657a
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-30 23:37:00 +0530

    Create RemoveDuplicates.java

---
+++ 1_interview/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        System.out.println("After removing Duplicates :: "+removeDuplicateString("bachchhav"));
+    }
+
+    public static String removeDuplicateString(String str){
+        StringBuffer sb = new StringBuffer();
+        HashSet<Character> exist = new HashSet<>();
+
+        for (char c : str.toCharArray()) {
+            if (!exist.contains(c)) {
+                exist.add(c);
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+
+    }
+}
