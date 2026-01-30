23427543d76a90a6227c812eefa09ffd86aaa086
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-01-30 23:36:55 +0530

    Create ListToSet.java

---
+++ 1_interview/ListToSet.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ListToSet {
+    public static void main(String[] args) {
+        List<String> list = List.of("Chetan","Bachchhav","Chetan","Patil","Chetan");
+
+        System.out.println("Original List :: "+list);
+
+        Set<String> set = new HashSet<>(list);
+        System.out.println("Set :: "+set);
+
+        List<String> setList = new ArrayList<>(set);
+        System.out.println("List :: "+new ArrayList<>(set));
+    }
+}
