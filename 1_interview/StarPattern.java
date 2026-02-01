cb57ca5ef8e0544cf8b9db85429bad9c4e935854
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-02-01 23:33:34 +0530

    Update StarPattern.java

--- 1_interview/StarPattern.java
+++ 1_interview/StarPattern.java
@@ -6,7 +6,6 @@ public class StarPattern {
         // ReverseTriangle(5);
         // triangleReverse(5);
         // emptySquare(5);
-        normalSquare(3);
     }
 
     public static void simpleSquare(int n) {
@@ -53,17 +52,6 @@ public class StarPattern {
                 }else{
                     System.out.print("  ");
                 }
-            }
-            System.out.println();
-        }
-    }
-
-    public static void normalSquare(int n){
-        int count = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(count+" ");
-                count++;
             }
             System.out.println();
         }
