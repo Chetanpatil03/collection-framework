407676944e8fcef6820204f0d9732613ecc4f616
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-02-03 23:50:42 +0530

    Update RemoveDuplicates.java

--- 1_interview/RemoveDuplicates.java
+++ 1_interview/RemoveDuplicates.java
@@ -1,13 +1,23 @@
+import java.security.KeyStore.Entry;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
         System.out.println("After removing Duplicates :: "+removeDuplicateString("bachchhav"));
+
+        showDuplicates(new int[]{2,5,3,7,8,9,6,2,8,9,6,4});
     }
 
     public static String removeDuplicateString(String str){
         StringBuffer sb = new StringBuffer();
         HashSet<Character> exist = new HashSet<>();
+        // ArrayList<Integer> duplicates = new ArrayList<>();
 
         for (char c : str.toCharArray()) {
             if (!exist.contains(c)) {
@@ -17,5 +27,31 @@ public class RemoveDuplicates {
         }
 
         return sb.toString();
+    }
+
+    public static void showDuplicates(int [] arr){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> dupli = new ArrayList<>();
+        List<Integer> unique = new ArrayList<>();
+        
+
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0)+1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() >= 2) {
+                dupli.add(entry.getKey());
+            }
+            else{
+                unique.add(entry.getKey());
+            }
+        }
+
+        Collections.sort(dupli);
+        Collections.sort(unique);
+
+        System.out.println("Duplicates :: "+dupli);
+        System.out.println("Unique :: "+unique);
     }
 }
