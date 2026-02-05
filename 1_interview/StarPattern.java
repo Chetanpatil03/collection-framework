fb81759a99e68323f88dd1d2b0f822c645a64587
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-02-05 23:58:05 +0530

    Update StarPattern.java

--- 1_interview/StarPattern.java
+++ 1_interview/StarPattern.java
@@ -78,6 +78,15 @@ public class StarPattern {
         }
     }
 
-    
+    public static void triangleNumberIncre(int n) {
+        int c = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(c+" ");
+                c++;
+            }
+            System.out.println();
+        }
+    }
 
 }
