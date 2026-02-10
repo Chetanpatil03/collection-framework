7bfd50a0482b98e3075cc30a7c32f4dfeb0dffeb
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-02-10 20:06:59 +0530

    Update CountingFrequency.java

--- 1_interview/CountingFrequency.java
+++ 1_interview/CountingFrequency.java
@@ -5,11 +5,13 @@ import java.util.Map;
 
 public class CountingFrequency {
     public static void main(String[] args) {
-        // int [] arr = {1,5,1,2,5,9,14,8,58,2,5,8,75,9,5,5,9,2};
+        int [] arr = {1,5,1,2,5,9,14,8,58,2,5,8,75,9,5,5,9,2};
         // System.out.println("Frequency of elements :: "+countFrequency(arr));
         // System.out.println("Frequency of elements :: "+countFrequencySorted(arr));
 
-        System.out.println("Frequency :: "+countCharacterFrequency("Chetan bachchhav "));
+        // System.out.println("Frequency :: "+countCharacterFrequency("Chetan bachchhav "));
+
+        numberWithHighestFrequency(arr);
     }
 
     public static Map countFrequency(int []arr){
@@ -33,6 +35,26 @@ public class CountingFrequency {
         for (char c : arr) map.put(c, map.getOrDefault(c, 0)+1);
 
         return map;
+    }
+
+    public static void numberWithHighestFrequency(int []arr){
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+
+        for (int i : arr) 
+            hashMap.put(i, hashMap.getOrDefault(i, 0)+1);
+
+        int freq = 1;
+        int num = arr[0];
+
+        for (Map.Entry<Integer,Integer> entry : hashMap.entrySet()) {
+            if (entry.getValue() > freq) {
+                freq = entry.getValue();
+                num = entry.getKey();
+            }
+        }
+
+
+        System.out.println("Number :: "+num+" | Occurance :: "+freq);
     }
 
 }
